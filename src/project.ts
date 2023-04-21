3faa381a756dfe653788413737ae73d5d2852a56
David <zapeexp@gmail.com>
2023-04-21 15:06:56 -0500

    added Vision

--- src/project.ts
+++ src/project.ts
@@ -1,7 +1,11 @@
 import {makeProject} from '@motion-canvas/core';
 
 import mision from './scenes/mision?scene';
+import vision from './scenes/vision?scene';
 
 export default makeProject({
-  scenes: [mision],
+  scenes: [
+    mision,
+    vision,
+  ],
 });
--- src/scenes/vision.tsx
+++ src/scenes/vision.tsx
@@ -2,8 +2,9 @@ import { variants } from "@catppuccin/palette";
 import { makeScene2D } from "@motion-canvas/2d";
 import { Img, Line, Rect, Txt } from "@motion-canvas/2d/lib/components";
 import { all } from "@motion-canvas/core/lib/flow";
+import { slideTransition } from "@motion-canvas/core/lib/transitions";
 import { easeInOutSine } from "@motion-canvas/core/lib/tweening";
-import { Vector2 } from "@motion-canvas/core/lib/types";
+import { Direction, Vector2 } from "@motion-canvas/core/lib/types";
 import { beginSlide, createRef } from "@motion-canvas/core/lib/utils";
 import { arcMove, scaleShow } from "../lib/transitions";
 
@@ -15,7 +16,7 @@ export default makeScene2D(function*(view) {
   const title = createRef<Rect>();
   const titlePos = Vector2.createSignal([0, 0]);
   const image = createRef<Img>();
-  const imagePos = Vector2.createSignal([400, 50]);
+  const imagePos = Vector2.createSignal([-400, 50]);
   const line = createRef<Line>();
   const lineText = createRef<Txt>();
 
@@ -23,7 +24,7 @@ export default makeScene2D(function*(view) {
     <>
       <Rect
         ref={title}
-        stroke={color.yellow.hex}
+        stroke={color.blue.hex}
         lineWidth={10}
         height={120}
         width={400}
@@ -33,15 +34,15 @@ export default makeScene2D(function*(view) {
         radius={20}
         position={titlePos}
       >
-        <Txt fill={color.yellow.hex}>Misión</Txt>
+        <Txt fill={color.blue.hex}>Visión</Txt>
       </Rect>
       <Img
         position={imagePos}
         ref={image}
-        src={"../../images/mision.jpg"}
+        src={"../../images/vision.avif"}
         size={[800, 500]}
         radius={20}
-        stroke={color.yellow.hex}
+        stroke={color.blue.hex}
         lineWidth={20}
         clip
       ></Img>
@@ -58,7 +59,7 @@ export default makeScene2D(function*(view) {
       ></Line>
       <Txt
         ref={lineText}
-        position={() => [titlePos().x + 250, imagePos().y - 50]}
+        position={() => [titlePos().x - 250, imagePos().y - 50]}
         fill={color.green.hex}
       >
         Se trata de
@@ -67,17 +68,17 @@ export default makeScene2D(function*(view) {
   );
 
   //hide elements
-  title().scale(0);
   image().scale(0);
   line().end(0);
   lineText().opacity(0);
   lineText().position.y(lineText().position.y() + 50);
 
+  //transitions
+  yield* slideTransition(Direction.Right);
   //animations
-  yield* scaleShow(title(), 2);
-
+  
   yield* beginSlide("mostrar imagen");
-  yield* arcMove(titlePos, [-600, -350], 1);
+  yield* arcMove(titlePos, [600, -350], 1);
   yield* line().end(1, 2, easeInOutSine);
   yield* all(
     lineText().position.y(lineText().position.y() - 50, 1),
